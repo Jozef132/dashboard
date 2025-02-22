4647c66ce930be5ef0609b015bda724e7fab7652
jozef132 <gemy16994@gmail.com>
2025-02-22 16:11:24 +0200

    database update

--- pages/api/expired_keys.js
+++ pages/api/expired_keys.js
@@ -1,12 +1,23 @@
 import admin from 'firebase-admin';
 
+// Initialize Firebase
 if (!admin.apps.length) {
+  const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAINL,
+  };
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
@@ -16,13 +27,15 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
       const expiredKeysSnapshot = await db.collection('expired_keys').get();
-      const expiredKeys = expiredKeysSnapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
+      const expiredKeys = [];
+      expiredKeysSnapshot.forEach((doc) => {
+        expiredKeys.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
       res.status(200).json(expiredKeys);
     } catch (err) {
-      console.error('Error fetching data:', err);
       res.status(500).json({ error: 'Failed to fetch expired keys' });
     }
   } else {
